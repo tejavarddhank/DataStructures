dbf9952a19accc3ad0542bfc11b5e25e775a6f81
tejavarddhank <102292970+tejavarddhank@users.noreply.github.com>
2023-06-05 09:51:49 +0530

    Create DeleteNNodesAfterMnodes.py

---
+++ DeleteNNodesAfterMnodes.py
@@ -0,0 +1,83 @@
+# your task is to complete this Function
+# Function shouldn't return anything
+
+'''
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def skipMdeleteN(self, head, M, N):
+        # Code here
+        h = head
+        if N == 0:
+            return head
+        k = 1
+        while h:
+            if k == M:
+                a = h
+                for _ in range(N):
+                    if a.next:
+                        a = a.next
+                    else:
+                        h.next = None
+                        return head
+                if a.next:
+                    h.next = a.next
+                    a.next = None
+                    k = 0 
+                else:
+                    h.next = None
+                    return head
+            h = h.next
+            k = k+1
+        return head
+
+
+
+#{ 
+ # Driver Code Starts
+#main
+
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to prit the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ") 
+            temp = temp.next
+        print("")
+
+
+if __name__=='__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = int(input())
+        m,p = list(map(int, input().strip().split()))
+        values = input().strip().split()
+        for i in reversed(values):
+            llist.push(i)
+        Solution().skipMdeleteN(llist.head, m, p)
+        llist.printList()
+        t -= 1
+# Contributed by: Harshit Sidhwa
+# } Driver Code Ends
