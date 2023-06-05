9d2191f8f0b1024a20fcbce66b0c7cb81f817b5a
tejavarddhank <102292970+tejavarddhank@users.noreply.github.com>
2023-06-05 09:46:09 +0530

    Create DeleteALinkedLisr.py

---
+++ DeleteALinkedLisr.py
@@ -0,0 +1,70 @@
+# your task is to complete this function
+# function should return new head pointer
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def delNode(head, k):
+    # Code here
+    a=head
+
+    if k==1:
+        return a.next
+    c=1
+    while a and a.next:
+        c=c+1
+        if c==k:
+            a.next=a.next.next
+        a=a.next
+    return head
+
+#{ 
+ # Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            temp = self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = new_node
+
+def printlist(head):
+    temp = head
+    while(temp):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        k = int(input())
+        newhead = delNode(list1.head, k)
+        printlist(newhead)
+# Contributed By: Harshit Sidhwa
+# } Driver Code Ends
