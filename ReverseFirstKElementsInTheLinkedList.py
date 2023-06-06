9b26a7141e33b5dbd13963eed0c1356740daecb8
tejavarddhank <102292970+tejavarddhank@users.noreply.github.com>
2023-06-06 15:50:03 +0530

    Create ReverseFirstKElementsInTheLinkedList.py

---
+++ ReverseFirstKElementsInTheLinkedList.py
@@ -0,0 +1,81 @@
+#function Template for python3
+
+"""
+# Node Class
+
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    #Function to reverse a linked list.
+    def reverseList(self, head, k):
+        # Code here
+        if head == None or k==0:
+            return head
+            
+        
+        head1 = None
+        c=0
+        while head != None and c<k:
+            temp = head 
+            head = head.next 
+            temp.next = head1
+            head1 = temp 
+            c+=1
+        
+        temp = head1    
+        while temp.next != None:
+            temp = temp.next
+            
+        temp.next = head 
+        head = head1
+        return head
+        
+            
+
+#{ 
+ # Driver Code Starts
+# Node Class    
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(head):
+    tmp = head
+    while tmp:
+        print(tmp.data, end=' ')
+        tmp=tmp.next
+    print()
+
+if __name__=='__main__':
+    for i in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        
+        lis = Linked_List()
+        for i in arr:
+            lis.insert(i)
+        
+        newHead = Solution().reverseList(lis.head)
+        printList(newHead)
+
+# } Driver Code Ends
